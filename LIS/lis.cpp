3b6d44f66d6ec2073f5d59044fc54093d6610e79
nacldoll82 <nacldoll82@gmail.com>
2017-08-28 02:02:56 -0400

    modified lis.cpp

--- LIS/lis.cpp
+++ LIS/lis.cpp
@@ -1,42 +1,36 @@
 #include <cstdio>
 #include <algorithm>
-using namespace std;
 
-int find_lis(int seq[], int seq_len)
+int lis(int *seq, int seq_len)
 {
-	int i, j, max_len;
-	int seq_val[501];
-	int *pos, pos_val;
+	int i, *pos;
+	int last[500], max_len;
 
 	max_len = 1;
-	seq_val[0] = seq[0];
+	last[0] = seq[0];
 
 	for(i=1; i<seq_len; ++i) {
-		pos = lower_bound(seq_val, seq_val + max_len, seq[i]);
+		pos = std::lower_bound(last, last+max_len, seq[i]);
 		*pos = seq[i];
-		max_len = max(max_len, int(pos - seq_val + 1));
-	}   
-
+		if(pos == last+max_len)
+			max_len++;
+	}
 	return max_len;
 }
 
 int main()
 {
-	int i, j;
-	int Tcase, seq_len, seq[500];
-	int result;
+	int tcase, i;
+	int seq_len, seq[500];
 
+	scanf("%d", &tcase);
 
-	scanf("%d", &Tcase);
-
-	while(Tcase--) {
+	while(tcase--) {
 		scanf("%d", &seq_len);
 		for(i=0; i<seq_len; ++i)
 			scanf("%d", &seq[i]);
 
-		result = find_lis(seq, seq_len);
-		printf("%d\n", result);
-	}   
-
+		printf("%d\n", lis(seq, seq_len));
+	}
 	return 0;
 }
